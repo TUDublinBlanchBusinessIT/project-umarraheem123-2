1c3b1b159744cb53874299c7769eea88d932ad15
Umar Raheem <you@example.comb00152481@mytudublin.ie>
2025-12-01 12:31:01 +0000

    Created FirebaseServices folder, working on importing using a service function

--- .expo/screens/CheckinScreen.js
+++ .expo/screens/CheckinScreen.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { db } from '../config/firebaseConfig';
 import { collection, addDoc } from "firebase/firestore";
+import { addCheckin } from "../services/firebaseService";
+
 
 
 export default function CheckInScreen() {
+    
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Check-In Screen</Text>
