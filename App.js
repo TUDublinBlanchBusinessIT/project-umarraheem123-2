e2e8d8f7dc912cc2d2af6c1522aed11a8ce26b8f
Umar Raheem <you@example.comb00152481@mytudublin.ie>
2025-12-08 01:02:27 +0000

    Initial Project Backup

--- App.js
+++ App.js
@@ -1,22 +1,42 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from './Screens/HomeScreen';
 import CheckinScreen from './Screens/CheckinScreen';
 import HistoryScreen from './Screens/HistoryScreen';
-import AdminScreen from './Screens/AdminScreen';  
+import AdminScreen from './Screens/AdminScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerTitleAlign: "center" }}>
 
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Checkin" component={CheckinScreen} />
-        <Stack.Screen name="History" component={HistoryScreen} />
-        <Stack.Screen name="Admin" component={AdminScreen} />
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: "Home" }}
+        />
+
+        <Stack.Screen 
+          name="Checkin" 
+          component={CheckinScreen} 
+          options={{ title: "Check-In" }}
+        />
+
+        <Stack.Screen 
+          name="History" 
+          component={HistoryScreen} 
+          options={{ title: "History" }}
+        />
+
+        <Stack.Screen 
+          name="Admin" 
+          component={AdminScreen} 
+          options={{ title: "Admin Panel" }}
+        />
 
       </Stack.Navigator>
     </NavigationContainer>
